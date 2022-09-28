c0316e63620712baffa3a02b4f5ca4038f9acb13
Nitika Saxena <97978599+nitika-saxena@users.noreply.github.com>
2022-09-28 22:35:18 +0530

    Create IndexOfTheFirstOccurrenceInString.java

---
+++ src/IndexOfTheFirstOccurrenceInString.java
@@ -0,0 +1,45 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+class IndexOfTheFirstOccurrenceInString {
+    public int strStr(String haystack, String needle) {
+        
+        if(needle.length()>haystack.length())
+            return -1;
+        for(int i =0; i< haystack.length() ; i++ ){
+            String match = "";
+            if(haystack.charAt(i)==(needle.charAt(0))){
+                try{
+                match = haystack.substring(i,i+needle.length());
+                }
+                catch(Exception e){
+                    return -1;
+                }
+            }
+            if(match.equalsIgnoreCase(needle))
+                return i;
+        }
+        return -1;
+    }
+}
